0d0379a1b8da2b3ab9507708151496d6b3c93c9c
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.2>
2024-04-28 15:41:09 -0600

    Learning activities 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,7 +3,29 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engenire";
+        job1._company = "Apple";
+        job1._startYear = "2020";
+        job1._endYear = "2024";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Microsoft";
+        job2._startYear = "2019";
+        job2._endYear = "2020";
+
+        //Console.WriteLine(job1._jobTitle);
+        //Console.WriteLine(job1._company);
+        //job1.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Ronal Orellana";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
