36caf6ee62c8342afcd60a10046f741f68e1fbd4
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.46>
2024-05-25 19:50:22 -0600

    Learning 04 inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assigment a1 = new Assigment("Ronal Orellana","Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssigment a2 = new MathAssigment("Roberto Rodriguez","Fractions","7.3","8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssigment a3 = new WritingAssigment("Mary Waters", "European History", "The Causes of Workd War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
---
+++ prepare/Learning04/WritingAssigment.cs
@@ -0,0 +1,16 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class WritingAssigment : Assigment
+{
+    private string _title;
+
+    public WritingAssigment(string studentName, string topic, string title) : base(studentName, topic){
+        _title = title;
+
+    }
+
+     public string GetWritingInformation(){
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+        }
+}
